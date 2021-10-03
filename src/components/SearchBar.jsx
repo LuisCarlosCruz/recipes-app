ffa858c1538ffe9347e419f76327eb16db7b9744
luis carlos <luizcruz0809@gmail.com>
2021-10-03 14:38:23 -0300

    acc

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-import React, { useContext } from 'react';
-import { Redirect } from 'react-router-dom';
-import Context from '../context/Context';
-=======
 import React from 'react';
->>>>>>> ee2db736490a31de67610bf9493be13c1f67cf12
 import SearchInputs from './SearchInputs';
 
 function SearchBar() {
