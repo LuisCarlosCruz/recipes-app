081f11100f22ef01bde1b574218a5c4b9234b68e
Letícia Castro <aicitelks@gmail.com>
2021-10-06 16:40:57 -0300

    Recupera a receita feita do local storage e seta no estado para renderizar no <RecipeDoneCard />

--- src/components/RecipeDoneCard.jsx
+++ src/components/RecipeDoneCard.jsx
@@ -1,10 +1,19 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import Context from '../context/Context';
 import RecipeDoneCardFilter from './RecipeDoneCardFilter';
 import RecipeDoneCardAll from './RecipeDoneCardAll';
 
 export default function RecipeDoneCard() {
-  const { allRecipesDone, filterRecipeDone } = useContext(Context);
+  const { allRecipesDone, setAllRecipesDone, filterRecipeDone } = useContext(Context);
+
+  useEffect(() => {
+    // recuperar do local storage
+    const recipeDone = JSON.parse(localStorage.getItem('doneRecipes'));
+    // setar no estado
+    setAllRecipesDone(recipeDone);
+    console.log('log no useEffect em RecipeDoneCard');
+    console.log(recipeDone);
+  }, [setAllRecipesDone]);
 
   // RETORNA COMPONENTE COM AS RECEITAS FILTRADAS
   if (filterRecipeDone.length !== 0) return (<RecipeDoneCardFilter />);
@@ -12,5 +21,5 @@ export default function RecipeDoneCard() {
   // RETORNA COMPONENTE COM TODAS AS RECEITAS FEITAS
   if (allRecipesDone.length !== 0) return (<RecipeDoneCardAll />);
 
-  return null;
+  return <p>Você não finalizou nenhuma receita</p>;
 } // end function
